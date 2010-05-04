41f3f69912b2e7191030c7e9165e1c90731f0aec
vvakame <vvakame+dev@gmail.com>
2010-05-04 12:07:52 +0900

    URL圧縮設定のEnabled制御を追加

--- src/net/vvakame/droppshare/activity/PreferencesActivity.java
+++ src/net/vvakame/droppshare/activity/PreferencesActivity.java
@@ -3,24 +3,40 @@ package net.vvakame.droppshare.activity;
 import net.vvakame.droppshare.R;
 import android.content.Context;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.preference.Preference.OnPreferenceChangeListener;
 
 /**
  * 設定用Activity
  * 
  * @author vvakame
  */
-public class PreferencesActivity extends PreferenceActivity {
+public class PreferencesActivity extends PreferenceActivity implements
+		OnPreferenceChangeListener {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.pref);
+
+		findPreference("uri_types").setOnPreferenceChangeListener(this);
+		findPreference("uri_shorten").setEnabled(
+				"http".equals(getUriType(this)));
+	}
+
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		if ("uri_types".equals(preference.getKey())) {
+			findPreference("uri_shorten").setEnabled("http".equals(newValue));
+		}
+
+		return true;
 	}
 
 	public static String getUriType(Context con) {
-		String key = "uti_types";
+		String key = "uri_types";
 		return PreferenceManager.getDefaultSharedPreferences(con).getString(
 				key, "http");
 	}
