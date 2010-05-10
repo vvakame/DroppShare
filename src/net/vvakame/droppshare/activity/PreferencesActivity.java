989a96f19c786aa8acdc4ec0265870932688a965
vvakame <vvakame+dev@gmail.com>
2010-05-10 22:42:35 +0900

    bug fix. VersionCode更新

--- src/net/vvakame/droppshare/activity/PreferencesActivity.java
+++ src/net/vvakame/droppshare/activity/PreferencesActivity.java
@@ -26,18 +26,7 @@ public class PreferencesActivity extends PreferenceActivity implements
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// v0.7→v0.8でCheckboxPreferenceからListPreferenceに変更したのの対応
-		Object tmp = PreferenceManager.getDefaultSharedPreferences(this)
-				.getAll().get(URI_SHORTEN);
-		if (tmp instanceof Boolean) {
-			boolean shorten = ((Boolean) tmp).booleanValue();
-
-			Editor editor = PreferenceManager.getDefaultSharedPreferences(this)
-					.edit();
-			editor.remove(URI_SHORTEN);
-			editor.putString(URI_SHORTEN, shorten ? "goo.gl" : "none");
-			editor.commit();
-		}
+		patchV08(this);
 
 		addPreferencesFromResource(R.xml.pref);
 
@@ -54,24 +43,23 @@ public class PreferencesActivity extends PreferenceActivity implements
 		return true;
 	}
 
-	public static String getUriType(Context con) {
-		String key = URI_TYPES;
-		return PreferenceManager.getDefaultSharedPreferences(con).getString(
-				key, "http");
+	public static String getUriType(Context context) {
+		return PreferenceManager.getDefaultSharedPreferences(context)
+				.getString(URI_TYPES, "http");
 	}
 
-	public static boolean isHttp(Context con) {
-		return "http".equalsIgnoreCase(getUriType(con));
+	public static boolean isHttp(Context context) {
+		return "http".equalsIgnoreCase(getUriType(context));
 	}
 
-	public static boolean isMarket(Context con) {
-		return "market".equalsIgnoreCase(getUriType(con));
+	public static boolean isMarket(Context context) {
+		return "market".equalsIgnoreCase(getUriType(context));
 	}
 
-	public static String getUriShortenAgent(Context con) {
-		String key = URI_SHORTEN;
-		return PreferenceManager.getDefaultSharedPreferences(con).getString(
-				key, "goo.gl");
+	public static String getUriShortenAgent(Context context) {
+		patchV08(context);
+		return PreferenceManager.getDefaultSharedPreferences(context)
+				.getString(URI_SHORTEN, "goo.gl");
 	}
 
 	public static boolean isAllowAutoCache(Context con) {
@@ -80,17 +68,16 @@ public class PreferencesActivity extends PreferenceActivity implements
 				key, true);
 	}
 
-	public static String getMessageTemplate(Context con) {
-		String key = MESSAGE_TEMPLATE;
-		String messageTemplate = con.getString(R.string.message_added);
-		return PreferenceManager.getDefaultSharedPreferences(con).getString(
-				key, messageTemplate);
+	public static String getMessageTemplate(Context context) {
+		String messageTemplate = context.getString(R.string.message_added);
+		return PreferenceManager.getDefaultSharedPreferences(context)
+				.getString(MESSAGE_TEMPLATE, messageTemplate);
 	}
 
-	public static String getDrozipName(Context con) {
+	public static String getDrozipName(Context context) {
 		String key = "drozip_name";
-		return PreferenceManager.getDefaultSharedPreferences(con).getString(
-				key, "archive");
+		return PreferenceManager.getDefaultSharedPreferences(context)
+				.getString(key, "archive");
 	}
 
 	public static boolean setDrozipName(Context con, String name) {
@@ -99,5 +86,20 @@ public class PreferencesActivity extends PreferenceActivity implements
 				.edit();
 		editor.putString(key, name);
 		return editor.commit();
+	}
+
+	private static void patchV08(Context context) {
+		// v0.7→v0.8でCheckboxPreferenceからListPreferenceに変更したのの対応
+		Object tmp = PreferenceManager.getDefaultSharedPreferences(context)
+				.getAll().get(URI_SHORTEN);
+		if (tmp instanceof Boolean) {
+			boolean shorten = ((Boolean) tmp).booleanValue();
+
+			Editor editor = PreferenceManager.getDefaultSharedPreferences(
+					context).edit();
+			editor.remove(URI_SHORTEN);
+			editor.putString(URI_SHORTEN, shorten ? "goo.gl" : "none");
+			editor.commit();
+		}
 	}
 }
