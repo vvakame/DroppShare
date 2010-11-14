e8b2044553348f897d1095ff4a733d7677fb9cc2
vvakame <vvakame+dev@gmail.com>
2010-11-14 19:18:31 +0900

    構成変更

--- Android/src/net/vvakame/util/shorten/NotShorten.java
+++
@@ -1,9 +0,0 @@
-package net.vvakame.util.shorten;
-
-public class NotShorten implements ShortenAgent {
-
-	@Override
-	public String getShorten(String uri) throws ShortenFailedException {
-		return uri;
-	}
-}
--- Android/src/net/vvakame/util/shorten/ShortenAgent.java
+++
@@ -1,5 +0,0 @@
-package net.vvakame.util.shorten;
-
-public interface ShortenAgent {
-	public String getShorten(String uri) throws ShortenFailedException;
-}
